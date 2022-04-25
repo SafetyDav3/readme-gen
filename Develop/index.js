1738f5950831587bb5e85d27eaa8f578d9d14159
SafteyDav3 <talktomedw@gmail.com>
2022-04-24 19:48:42 -0500

    Question and response working

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,26 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const prompt = inquirer.createPromptModule();
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questionsArr = [
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "What is the title of your project?: ",
+    default: "Generic Name",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  prompt(questionsArr).then(function (answers) {
+    console.log(answers);
+  });
+}
 
 // Function call to initialize app
 init();
