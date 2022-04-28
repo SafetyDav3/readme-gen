8c6d1377beae71e96b25d302db92ae1bcd6a0850
SafteyDav3 <talktomedw@gmail.com>
2022-04-28 02:55:27 -0500

    License info and links added

--- Develop/utils/readme-template.js
+++ Develop/utils/readme-template.js
@@ -1,4 +1,26 @@
 const readmeTemplate = function (answers) {
+  let linLink = "";
+
+  if (answers.license === "MIT") {
+    linLink = `[${answers.license}](https://opensource.org/licenses/MIT)`;
+  } else if (answers.license === "GPLv2") {
+    linLink = `[${answers.license}](https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)`;
+  } else if (answers.license === "GPLv3") {
+    linLink = `[${answers.license}](https://www.gnu.org/licenses/gpl-3.0.en.html)`;
+  } else if (answers.license === "Apache") {
+    linLink = `[${answers.license}](https://www.apache.org/licenses/LICENSE-2.0)`;
+  } else if (answers.license === "BSD 3-clause") {
+    linLink = `[${answers.license}](https://opensource.org/licenses/BSD-3-Clause)`;
+  } else if (answers.license === "BSD 2-clause") {
+    linLink = `[${answers.license}](https://opensource.org/licenses/BSD-2-Clause)`;
+  } else if (answers.license === "LGPLv3") {
+    linLink = `[${answers.license}](https://www.gnu.org/licenses/lgpl-3.0.en.html)`;
+  } else if (answers.license === "AGPLv3") {
+    linLink = `[${answers.license}](https://www.gnu.org/licenses/agpl-3.0.en.html)`;
+  } else {
+    linLink = answers.license;
+  }
+
   return `
           
 # ${answers.projectTitle}
@@ -27,6 +49,7 @@ ${answers.tests}
 
 ## This app falls under these license agreements
 ${answers.license}
+More information can be found here: ${linLink}
 
 ## Additional contributors to this app
 ${answers.contributorName}
